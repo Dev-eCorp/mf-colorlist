77b276678a9ebbd800a37f493497e612dcc9f74b
Eduard Cruz <eduard@nebula.com.co>
2023-07-28 04:05:04 -0500

    Se agrega boton de limpiar lista de colores

--- src/components/ColorList.jsx
+++ src/components/ColorList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Swal from "sweetalert2";
 
-const ColorList = ({ colorList = [] }) => {
+const ColorList = ({ colorList = [], handleClickClearColors }) => {
 	const handleCopyColor = (color) => {
 		navigator.clipboard.writeText(color);
 
@@ -16,30 +16,40 @@ const ColorList = ({ colorList = [] }) => {
 	};
 
 	return (
-		<div className="list-group text-center">
-			{colorList.length > 0 ? (
-				colorList.map((color, index) => (
-					<button
-						key={index}
-						type="button"
-						className="list-group-item list-group-item-action text-white"
-						aria-current="true"
-						title="Copy"
-						style={{
-							backgroundColor: color,
-							fontWeight: "bold",
-						}}
-						onClick={() => handleCopyColor(color)}
-					>
-						{color}
-					</button>
-				))
-			) : (
-				<div className="alert alert-danger" role="alert">
-					<b>No colors selected</b>
-				</div>
+		<>
+			{colorList.length > 0 && (
+				<button
+					className="btn btn-danger my-4 w-100"
+					onClick={handleClickClearColors}
+				>
+					Clear list
+				</button>
 			)}
-		</div>
+			<div className="list-group text-center">
+				{colorList.length > 0 ? (
+					colorList.map((color, index) => (
+						<button
+							key={index}
+							type="button"
+							className="list-group-item list-group-item-action text-white"
+							aria-current="true"
+							title="Copy"
+							style={{
+								backgroundColor: color,
+								fontWeight: "bold",
+							}}
+							onClick={() => handleCopyColor(color)}
+						>
+							{color}
+						</button>
+					))
+				) : (
+					<div className="alert alert-danger" role="alert">
+						<b>No colors selected</b>
+					</div>
+				)}
+			</div>
+		</>
 	);
 };
 
